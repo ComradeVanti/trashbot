a861826c592daacbb8c3dd2c0090b8efc5158e5b
ComradeVanti <kingvanti650@gmail.com>
2022-05-16 14:45:33 +0200

    Add socket server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,20 @@
 import express from "express";
+import {createServer} from "http";
+import {Server} from "socket.io"
 
 const port = 3000
 
 const app = express()
+const httpServer = createServer(app);
+const io = new Server(httpServer)
 
 app.get('/ping', (req, res) => {
     res.send('pong');
 });
 
-app.listen(port, () => {
+io.on("connection", socket => {
+})
+
+httpServer.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
 });
