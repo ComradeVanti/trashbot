584c7e556cf8566ead3229a37b112daf614d69c4
ComradeVanti <kingvanti650@gmail.com>
2022-05-16 14:04:55 +0200

    Add ping end-point to server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,13 @@
-console.log("hello world")
+import express from "express";
+
+const port = 3000
+
+const app = express()
+
+app.get('/ping', (req, res) => {
+    res.send('pong');
+});
+
+app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+});
