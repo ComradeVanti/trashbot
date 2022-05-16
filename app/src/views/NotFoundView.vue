b8e60eb4be58a903bf147f3b857ffece38f94751
Michelle Markl <65341595+MMischi@users.noreply.github.com>
2022-05-16 15:20:29 +0200

    Add not found view
    A view has been added which notifies the user if the specified path does not exist. The view contains a link to return to the home page.

---
+++ app/src/views/NotFoundView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>Seite nicht gefunden!</div>
+  <router-link to="/">Home</router-link>
+</template>
+
+<script>
+export default {
+  name: "NotFoundView",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style></style>
