029d579481f24a95b611797e1bdf9c22cfc74585
Michelle Markl <65341595+MMischi@users.noreply.github.com>
2022-05-17 17:12:10 +0200

    Add enemy pin component
    This component will be used later to show the opponents/teammates on the map (red robot head).

---
+++ app/src/components/EnemyPin.vue
@@ -0,0 +1,25 @@
+<template>
+  <GMapMarker
+    class="marker"
+    :position="position"
+    :icon="{
+      url: img,
+      scaledSize: { width: 45, height: 40 },
+    }"
+  />
+</template>
+
+<script>
+const img_path = await import("../assets/img/EnemyBot.png");
+
+export default {
+  name: "EnemyPin",
+  setup() {
+    return {
+      img: img_path.default,
+    };
+  },
+};
+</script>
+
+<style></style>
