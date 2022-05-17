9f103dfaeae5257b86f935ad8e4441bdc209f0db
Eva <evamaria.altenburger@gmail.com>
2022-05-17 13:59:07 +0200

    Add InputField Component

---
+++ app/src/components/InputField.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="input-group mb-3">
+    <input
+      type="text"
+      class="form-control"
+      :placeholder="placeholder"
+      :aria-label="label"
+      aria-describedby="basic-addon1"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InputField",
+  props: {
+    placeholder: {
+      type: String,
+      default: "",
+    },
+    label: {
+      type: String,
+      default: "text",
+    },
+  },
+};
+</script>
+
+<style></style>
