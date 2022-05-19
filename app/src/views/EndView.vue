9565c051c764f63801515d4797283d2579336420
Eva <evamaria.altenburger@gmail.com>
2022-05-19 12:53:44 +0200

    Add empty EndView witout content or logic

---
+++ app/src/views/EndView.vue
@@ -0,0 +1,22 @@
+<template>
+  <p>Endscreen</p>
+</template>
+
+<script>
+export default {
+  name: "EndView",
+  created() {
+    document.addEventListener("beforeunload", this.deleteStorage);
+  },
+  methods: {
+    deleteStorage() {
+      localStorage.clear();
+    },
+  },
+};
+
+//allPlayers + their stats for basic + items if more
+</script>
+
+<style scoped>
+</style>
