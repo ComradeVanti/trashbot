1b8559aea802f908d2498b2d7e887191313a8b56
ComradeVanti <kingvanti650@gmail.com>
2022-05-20 10:31:08 +0200

    Redraw lobby

--- app/src/components/ButtonComp.vue
+++ app/src/components/ButtonComp.vue
@@ -13,7 +13,6 @@
   font-size: var(--fnt-sze-label);
   box-shadow: var(--sdw-regular);
   border-radius: var(--dim-corners);
-  font-family: var(--fnt-btn);
   padding: var(--dim-small) var(--dim-regular);
   border: none;
   width: var(--dim-btn);
